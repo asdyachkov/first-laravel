acb6413cf2436e0e27ce4003602910096c06cfee
Stepa) <stepanderdichok@gmail.com>
2023-01-07 04:00:51 +0300

    lab17

--- routes/web.php
+++ routes/web.php
@@ -6,54 +6,54 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CommentController;
 
-Route::get('/', [ArticleController::class, 'index']);
-
-//Route::get('/list', function () {
-//    $list = json_decode(file_get_contents(public_path().'/articles.json'), true);
-//    return view('list', ['list'=>$list]);
+//Route::get('/', [ArticleController::class, 'index']);
+//
+////Route::get('/list', function () {
+////    $list = json_decode(file_get_contents(public_path().'/articles.json'), true);
+////    return view('list', ['list'=>$list]);
+////});
+////
+//Route::get('/about', function () {
+//    return view('about');
 //});
 //
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/galery/{full}', [MainController::class, 'show']);
+//Route::get('/galery/{full}', [MainController::class, 'show']);
+////
+////Route::get('/post/{id}', function ($id) {
+////    $list = json_decode(file_get_contents(public_path().'/articles.json'), true);
+////    return view('post', ['list'=>$list, 'id'=>$id]);
+////});
 //
-//Route::get('/post/{id}', function ($id) {
-//    $list = json_decode(file_get_contents(public_path().'/articles.json'), true);
-//    return view('post', ['list'=>$list, 'id'=>$id]);
+//Route::get('/contacts', function () {
+//    $contacts = $contact = [
+//        'name' => 'Политех',
+//        'adres' => 'Пряники',
+//        'phone' => '8(495)423-2323',
+//        'email' => '@mospolytech.ru',
+//    ];
+//    return view('/contacts', ['contacts' => $contacts]);
 //});
-
-Route::get('/contacts', function () {
-    $contacts = $contact = [
-        'name' => 'Политех',
-        'adres' => 'Пряники',
-        'phone' => '8(495)423-2323',
-        'email' => '@mospolytech.ru',
-    ];
-    return view('/contacts', ['contacts' => $contacts]);
-});
-
-//Auth
-Route::get('/auth/registr', [AuthController::class, 'create']);
-Route::post('/auth/registr', [AuthController::class, 'store']);
-Route::get('/auth/login', [AuthController::class, 'login'])->name('login');
-Route::post('/auth/login', [AuthController::class, 'customLogin']);
-Route::get('/auth/logout', [AuthController::class, 'logout']);
-
-
-//Article
-Route::group(['prefix'=>'/article', 'middleware'=>'auth:sanctum'], function(){
-    Route::get('/create', [ArticleController::class, 'create']);
-    Route::post('/store', [ArticleController::class, 'store']);
-    Route::get('/show/{id}', [ArticleController::class, 'show'])->name('show')->middleware('path');
-    Route::get('/{id}/edit', [ArticleController::class, 'edit']);
-    Route::put('/{id}', [ArticleController::class, 'update']);
-    Route::get('/{id}/delete', [ArticleController::class, 'destroy']);
-});
-
-
-//Comment
-Route::resource('comment', CommentController::class);
-Route::get('/comment/{comment}/accept', [CommentController::class, 'accept']);
-Route::get('/comment/{comment}/reject', [CommentController::class, 'reject']);
+//
+////Auth
+//Route::get('/auth/registr', [AuthController::class, 'create']);
+//Route::post('/auth/registr', [AuthController::class, 'store']);
+//Route::get('/auth/login', [AuthController::class, 'login'])->name('login');
+//Route::post('/auth/login', [AuthController::class, 'customLogin']);
+//Route::get('/auth/logout', [AuthController::class, 'logout']);
+//
+//
+////Article
+//Route::group(['prefix'=>'/article', 'middleware'=>'auth:sanctum'], function(){
+//    Route::get('/create', [ArticleController::class, 'create']);
+//    Route::post('/store', [ArticleController::class, 'store']);
+//    Route::get('/show/{id}', [ArticleController::class, 'show'])->name('show')->middleware('path');
+//    Route::get('/{id}/edit', [ArticleController::class, 'edit']);
+//    Route::put('/{id}', [ArticleController::class, 'update']);
+//    Route::get('/{id}/delete', [ArticleController::class, 'destroy']);
+//});
+//
+//
+////Comment
+//Route::resource('comment', CommentController.php::class);
+//Route::get('/comment/{comment}/accept', [CommentController.php::class, 'accept']);
+//Route::get('/comment/{comment}/reject', [CommentController.php::class, 'reject']);
